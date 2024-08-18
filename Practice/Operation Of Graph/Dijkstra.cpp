b24a1d171a96c5b83e98e4b1cdd515f35bdd2345
issunyi <issunsun1@gmail.com>
2024-08-18 21:51:45 +0800

    20240818

--- Practice/Operation Of Graph/Dijkstra.cpp
+++ Practice/Operation Of Graph/Dijkstra.cpp
@@ -6,73 +6,33 @@
 #include <stack>
 
 using namespace std;
+const int N = 10000;
 
-class Solution
+int v[N];
+int dis[N];
+int a[N][N];
+
+// 单源正权值最短路径
+void dijkstra(int n)
 {
-public:
-    int networkDelayTime(vector<vector<int>> &times, int n, int k)
+    memset(dis, INT_MAX, sizeof(dis)); // 初始化
+    v[1] = 1;
+    dis[1] = 0;
+
+    for (int i = 1; i <= n; ++i)
     {
-        // 定义图和访问数组、距离数组
-        vector<vector<long long>> graph(n + 1, vector<long long>(n + 1, INT_MAX));
-        vector<long long> distance(n + 1, INT_MAX);
-        vector<bool> visited(n + 1, false);
-        distance[k] = 0;
-        // 初始化图中节点的距离
-        for (int i = 1; i <= n; i++)
-        {
-            graph[i][i] = 0;
-        }
-        for (auto &time : times)
-        {
-            graph[time[0]][time[1]] = time[2];
-        }
+        int k = 0;
 
-        // n个点，遍历n次
-        for (int i = 0; i < n; i++)
-        {
-            int index = -1, minNum = INT_MAX;
-            // 找最近的点
-            for (int j = 1; j <= n; j++)
-            {
-                if (!visited[j] && distance[j] < minNum)
-                {
-                    index = j;
-                    minNum = distance[j];
-                }
-            }
-            // 都遍历过直接退出
-            if (index == -1)
-            {
-                break;
-            }
-            // 标记当前点访问过
-            visited[index] = true;
-            // 更新到别的点的最短距离
-            for (int v = 1; v <= n; v++)
-            {
-                if (!visited[v] && graph[index][v] != INT_MAX)
-                {
-                    if (distance[index] + graph[index][v] < distance[v])
-                    {
-                        distance[v] = distance[index] + graph[index][v];
-                    }
-                }
-            }
-        }
+        for (int j = 1; j <= n; ++j) // 找出距离最近的点
+            if (!v[j] && (k == 0 || dis[j] < dis[k]))
+                k = j;
+        v[k] = 1; // 加入集合
 
-        int ans = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            // k到i点没有路径
-            if (distance[i] == INT_MAX)
-            {
-                return -1;
-            }
-            ans = max(ans, (int)distance[i]);
-        }
-        return ans;
+        for (int j = 1; j <= n; ++j) // 松弛
+            if (!v[j])
+                dis[j] = min(dis[j], dis[k] + a[k][j]);
     }
-};
+}
 
 int main()
 {
---
+++ Practice/Operation Of Graph/Floyd.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+void floydWarshall(vector<vector<int>> &graph)
+{
+    int V = graph.size();
+    vector<vector<int>> dist = graph;
+
+    // Floyd算法的核心部分
+    for (int k = 0; k < V; k++)
+    {
+        for (int i = 0; i < V; i++)
+        {
+            for (int j = 0; j < V; j++)
+            {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    // // 打印结果
+    // for (int i = 0; i < V; i++)
+    // {
+    //     for (int j = 0; j < V; j++)
+    //     {
+    //         if (dist[i][j] == INT_MAX)
+    //         {
+    //             cout << "INF ";
+    //         }
+    //         else
+    //         {
+    //             cout << dist[i][j] << "   ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+}
+
+int main()
+{
+    vector<vector<int>> graph = {
+        {0, 3, INT_MAX, 7},
+        {8, 0, 2, INT_MAX},
+        {5, INT_MAX, 0, 1},
+        {2, INT_MAX, INT_MAX, 0}};
+
+    floydWarshall(graph);
+
+    return 0;
+}
