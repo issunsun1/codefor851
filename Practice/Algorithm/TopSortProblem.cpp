13638432c3a48d5af68ea1b6971f7d8731d2cee8
issunyi <issunsun1@gmail.com>
2024-09-04 22:32:37 +0800

    20240904

---
+++ Practice/Algorithm/TopSortProblem.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+// 拓扑排序应用
+
+vector<int> findtopsort(vector< vector<int> > & course,int coursenum)
+{
+    vector< vector<int> > neighbor;
+    vector<int> ans;
+    vector<int> incount(coursenum, 0);
+    queue<int> q;
+
+    int n = course.size();
+    int m = course.size();
+
+    for (int i = 0;i < n;i++)
+    {
+        neighbor[course[i][1]].push_back(neighbor[course[i][0]]);
+        incount[course[i][0]]++;
+    }
+
+    for (int i = 0; i < n;i++)
+        if(incount[i]==0)
+            q.push(i);
+
+    while(!q.empty())
+    {
+        int temp = q.front();
+        q.pop();
+        ans.push_back(temp);
+
+        for (int i = 0; i < neighbor[temp].size();i++)
+        {
+            incount[neighbor[temp][i]]--;
+            if(incount[neighbor[temp][i]]==0)
+                q.push(i);
+        }
+    }
+
+    if(ans.size()!=coursenum)
+        return ans;  //返回空向量
+    return ans;
+}
