7563a7253525722bc4ca9e7c60b499db2996d423
issunyi <issunsun1@gmail.com>
2024-09-05 22:22:35 +0800

    Create 2010B.cpp

---
+++ Codeforce/2010B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << 3 - a - b;
+    return 0;
+}
