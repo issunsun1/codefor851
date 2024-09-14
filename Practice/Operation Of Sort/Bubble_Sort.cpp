0398cf860c0e8b0c69d7c94b88a8aaa18384b957
issunyi <issunsun1@gmail.com>
2024-09-14 22:41:37 +0800

    Update Bubble_Sort.cpp

--- Practice/Operation Of Sort/Bubble_Sort.cpp
+++ Practice/Operation Of Sort/Bubble_Sort.cpp
@@ -13,12 +13,23 @@ const int N = 1000;
 // 平均时间复杂度 O(n^2)
 // 是一种稳定的排序算法
 
+// 排序过程：每次确定一个最大的元素放在数组结尾
+
 void bubble_sort(int a[N], int len)
 {
     for (int i = 0; i < len - 1; i++)
         for (int j = 0; j < len - 1 - i; j++)
             if (a[j] > a[j + 1])
                 swap(a[j], a[j + 1]);
+
+}
+
+void bubble(vector<int> a)
+{
+    for (int i = 0; i < a.size() - 1;i++)
+        for (int j = 0; j < a.size() - 1 - i;j++)
+                if(a[j]>a[j+1])
+                    swap(a[j], a[j + 1]);
 }
 
 int main()
