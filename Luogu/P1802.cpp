014e1608a7d69047b6b02aa6517ddae28cb544c9
issunyi <issunsun1@gmail.com>
2024-09-23 21:34:50 +0800

    20240923

---
+++ Luogu/P1802.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int  main()
+{
+    int n,x;
+    cin>>n>>x;
+    for(int i = 0;i < n;i++)
+    {
+        
+    }
+
+}
