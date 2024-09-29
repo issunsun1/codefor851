9df8d860426f432e134d9187fa87e703031f5c75
issunyi <issunsun1@gmail.com>
2024-09-29 21:54:51 +0800

    20240929

--- Practice/Operation Of List/List2SearchTree.cpp
+++ Practice/Operation Of List/List2SearchTree.cpp
@@ -8,6 +8,7 @@ typedef struct TreeNode{
     int val;
     TreeNode *left;
     TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {};
 } TreeNode;
 
 typedef struct ListNode
