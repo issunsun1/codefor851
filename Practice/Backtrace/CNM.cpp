3b51fab45e386737c269bffb6dfe3f699c6236cc
issunyi <issunsun1@gmail.com>
2024-11-10 17:16:08 +0800

    Update CNM.cpp

--- Practice/Backtrace/CNM.cpp
+++ Practice/Backtrace/CNM.cpp
@@ -7,29 +7,27 @@
 
 using namespace std;
 
-class Solution
-{
-public:
-    vector< vector<int> > permute(vector<int> &nums)
-    {
-        dfs(nums, 0);
-        return res;
-    }
+vector< vector<int> > res;
 
-private:
-    vector< vector<int> > res;
-    void dfs(vector<int> &nums, int x)
+void dfs(vector<int> &nums, int x)
+{
+    if (x == nums.size() - 1)
     {
-        if (x == nums.size() - 1)
-        {
-            res.push_back(nums); // 添加排列方案
-            return;
-        }
-        for (int i = x; i < nums.size(); i++)
-        {
-            swap(nums[i], nums[x]); // 交换，将 nums[i] 固定在第 x 位
-            dfs(nums, x + 1);       // 开启固定第 x + 1 位元素
-            swap(nums[i], nums[x]); // 恢复交换
-        }
+        res.push_back(nums); // 添加排列方案
+        return;
     }
-};
+    for (int i = x; i < nums.size(); i++)
+    {
+        swap(nums[i], nums[x]); // 交换，将 nums[i] 固定在第 x 位
+        dfs(nums, x + 1);       // 开启固定第 x + 1 位元素
+        swap(nums[i], nums[x]); // 恢复交换
+    }
+}
+
+vector< vector<int> > permute(vector<int> &nums)
+{
+    dfs(nums, 0);
+    return res;
+}
+
+
