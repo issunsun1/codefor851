20ad4c4518cf39894269e9081c7fcd68f2dbfed5
issunyi <issunsun1@gmail.com>
2024-11-24 20:46:13 +0800

    1

--- Practice/Algorithm/01_Bag.cpp
+++ Practice/Algorithm/01_Bag.cpp
@@ -17,29 +17,25 @@ int bagsize;
 
 int resolution()
 {
-    vector< vector<int> > a(weight.size(), vector<int>(bagsize + 1, 0));
-    int m = a.size();
-    int n = a[0].size();
+    int m = weight.size(); // 物品数量
+    int n = bagsize + 1;   // 背包容量 + 1
 
-    vector<int> value;
+    vector< vector<int> > a(m + 1, vector<int>(n, 0)); // 创建二维数组，初始化为0
 
-    for (int i = 1; i < n; i++)
-        a[0][i] = value[0];
-
-    for (int i = 1; i < m; i++)
-        for (int j = 1; j < n; j++)
+    // 填充动态规划数组
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= bagsize; j++)
         {
-            if (j < weight[i])
-                a[i][j] = a[i - 1][j];
+            if (j < weight[i - 1])     // 如果背包容量小于当前物品的重量，不能放入该物品
+                a[i][j] = a[i - 1][j]; // 继承前一行的最大值
             else
-                a[i][j] = max(a[i - 1][j], a[i][j - weight[i]] + value[i]);
+                // 选择放入或不放入当前物品，取最大值
+                a[i][j] = max(a[i - 1][j], a[i - 1][j - weight[i - 1]] + value[i - 1]);
         }
+    }
 
-    int maxn = 0;
-    for (int i = 0; i < m; i++)
-        maxn = max(maxn, a[i][n - 1]);
-
-    return maxn;
+    return a[m][bagsize]; // 返回背包容量为 bagsize 时的最大价值
 }
 
 int main()
--- Practice/Algorithm/01_Bag_mult.cpp
+++ Practice/Algorithm/01_Bag_mult.cpp
@@ -12,29 +12,30 @@ int bagsize;
 
 // 多重背包问题
 
-int resolution()
-{
-    
-    vector< vector<int> > a(weight.size(), vector<int>(bagsize + 1, 0));
 
-    for (int i = 0; i < bagsize + 1; i++)
-        a[0][i] = value[0];
+int resolution() {
+    int n = weight.size();  // 物品数量
+    vector< vector<int> > dp(n + 1, vector<int>(bagsize + 1, 0));  // 动态规划表
 
-    for (int i = 1; i < weight.size(); i++)
-        for (int j = 1; j < bagsize + 1; j++)
-        {
-            if (j < bagsize)
-                a[i][j] = a[i - 1][j];
-            else
-                for (int k = 1; k <= nums[i] && k * weight[i] <= j; k++)
-                    a[i][j] = max(a[i - 1][j], a[i][j - weight[i] * k] + value[i] * k);
+    // 初始化第一行的状态
+    for (int i = 0; i <= bagsize; i++) {
+        dp[0][i] = 0;  // 物品0的情况（没有物品，价值为0）
+    }
+
+    // 从第1个物品开始
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= bagsize; j++) {
+            dp[i][j] = dp[i - 1][j];  // 不选当前物品时的最大值
+
+            // 尝试选当前物品的不同数量
+            for (int k = 1; k <= nums[i - 1] && k * weight[i - 1] <= j; k++) {
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - k * weight[i - 1]] + k * value[i - 1]);
+            }
         }
+    }
 
-    int maxn = 0;
-    for (int i = 0; i < weight.size(); i++)
-        maxn = max(maxn, a[i][bagsize]);
-
-    return maxn;
+    // 返回最大价值
+    return dp[n][bagsize];
 }
 
 int main()
