181df8e3ca97c28add94d93b2dbbe4645f2a3758
issunyi <issunsun1@gmail.com>
2024-12-16 22:49:05 +0800

    Update MostNum.cpp

--- Practice/Algorithm/MostNum.cpp
+++ Practice/Algorithm/MostNum.cpp
@@ -8,28 +8,28 @@
 
 using namespace std;
 
-bool static cmp(const pair<int,int>a,const pair<int,int> b)
+bool static cmp(const pair<int, int> a, const pair<int, int> b)
 {
     return a.second > b.second;
 }
 
 vector<int> search_num(vector<int> a)
 {
-    unordered_map<int,int> map;
+    unordered_map<int, int> map;
     vector<int> res;
 
-    for (int i = 0; i < a.size();i++)
+    for (int i = 0; i < a.size(); i++)
         map[a[i]]++;
 
-    vector< pair<int, int> > b(map.begin(),map.end());
+    vector<pair< int, int> > b(map.begin(), map.end());
 
-    sort(b.begin(),b.end(), cmp);
+    sort(b.begin(), b.end(), cmp);
 
     res.push_back(b[0].first);
 
-    for (int i = 1; i < b.size();i++)
+    for (int i = 1; i < b.size(); i++)
     {
-        if(b[i].second==b[0].second)
+        if (b[i].second == b[0].second)
             res.push_back(b[i].first);
         else
             break;
@@ -38,8 +38,33 @@ vector<int> search_num(vector<int> a)
     return res;
 }
 
+// 摩尔投票法
+int majorityElement(vector<int> &nums)
+{
+    int candidate = nums[0];
+    // 选票
+    int count = 0;
+    for (int voter : nums)
+    {
+        if (voter == candidate)
+        {
+            count++;
+        }
+        else if (voter != candidate && count == 0)
+        {
+            candidate = voter;
+            count++;
+        }
+        else if (voter != candidate && count != 0)
+        {
+            count--;
+        }
+    }
+    return candidate;
+}
+
 int main()
 {
-    
+
     return 0;
 }
