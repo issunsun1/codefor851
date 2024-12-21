b6c360328048db681aadc553acf32d69d568741e
issunyi <issunsun1@gmail.com>
2024-12-21 21:57:55 +0800

    2

--- Practice/Algorithm/01_Bag_mult.cpp
+++ Practice/Algorithm/01_Bag_mult.cpp
@@ -17,19 +17,14 @@ int resolution() {
     int n = weight.size();  // 物品数量
     vector< vector<int> > dp(n + 1, vector<int>(bagsize + 1, 0));  // 动态规划表
 
-    // 初始化第一行的状态
-    for (int i = 0; i <= bagsize; i++) {
-        dp[0][i] = 0;  // 物品0的情况（没有物品，价值为0）
-    }
-
     // 从第1个物品开始
     for (int i = 1; i <= n; i++) {
-        for (int j = 0; j <= bagsize; j++) {
+        for (int j = 1; j <= bagsize; j++) {
             dp[i][j] = dp[i - 1][j];  // 不选当前物品时的最大值
 
             // 尝试选当前物品的不同数量
             for (int k = 1; k <= nums[i - 1] && k * weight[i - 1] <= j; k++) {
-                dp[i][j] = max(dp[i-1][j], dp[i - 1][j - k * weight[i - 1]] + k * value[i - 1]);
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - k * weight[i - 1]] + k * value[i - 1]);
             }
         }
     }
